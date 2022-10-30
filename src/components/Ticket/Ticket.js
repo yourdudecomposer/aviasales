b410f53d68e5f72007dfe356f7dd455dcc6b6ed7
Filipp Goriainov <barrios1991phil@gmail.com>
2022-10-30 20:44:11 +0400

    make time

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -6,7 +6,7 @@ function Ticket({price,segments,carrier}) {
     return (<section className={classes['ticket']}>
         <header>
             <p>{price.toLocaleString()} p</p>
-            <img src={`https://daisycon.io222/images/airline/?width=100&height=36&color=ffffff&iata=${carrier}`} alt="logo" />
+            <img src={`https://daisycon.io/images/airline/?width=100&height=36&color=ffffff&iata=${carrier}`} alt="logo" />
         </header>
         <article className={classes['segments']}>
             <TicketSegment segment={segments[0]}/>
